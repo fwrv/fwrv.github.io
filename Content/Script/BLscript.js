483c7e63e0bac1c0f783c59c4304cb0c0e662f25
fwrv <fernando.rodriguez.villa@gmail.com>
2016-02-18 10:12:50 +0000

    youtube iframe

--- Content/Script/BLscript.js
+++ Content/Script/BLscript.js
@@ -1,9 +1,33 @@
 window.onload = function() {
 	document.getElementById("myBtn").onclick = myFunction;
-}
+
+	var tag = document.createElement('script');
+
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 
 function myFunction() {
   	alert("Nice! You clicked on the button!");
+}
+
+/* YouTube iFrame fooling around */
+
+var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
 }
 
 /* Method for blog Posts */

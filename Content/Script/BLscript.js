2fd1cacbd3a70a1f70a013a07ee8f609bd8a25e1
fwrv <fernando.rodriguez.villa@gmail.com>
2016-02-18 15:42:56 +0000

    google maps

--- Content/Script/BLscript.js
+++ Content/Script/BLscript.js
@@ -1,22 +1,37 @@
 window.onload = function() {
 	document.getElementById("myBtn").onclick = myFunction;
-
-	var tag = document.createElement('script');
-
-	tag.src = "https://www.youtube.com/iframe_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
+}
+	
+/* Button function */
 function myFunction() {
   	alert("Nice! You clicked on the button!");
 }
 
-/* YouTube iFrame fooling around */
+
+/* Method for blog Posts NOT IN USE*/
+function blogPost(title,tag,summary,content,datePosted,author) {
+	this.title = title;
+	this.tag = tag;
+	this.summary = summary;
+	this.content = content;
+	this.datePosted = datePosted;
+	this.author = author;
+};
+
+/* how to input new blog posts NOT IN USE*/
+var Post1 = new blogPost("Post1");
+
+
+/* YouTube iFrame novistry NOT IN USE*/
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
-      function onYouTubeIframeAPIReady() {
+    function onYouTubeIframeAPIReady() {
         player = new YT.Player('player', {
           height: '390',
           width: '640',
@@ -27,18 +42,3 @@ var player;
           }
         });
       }
-
-}
-
-/* Method for blog Posts */
-function blogPost(title,tag,summary,content,datePosted,author) {
-	this.title = title;
-	this.tag = tag;
-	this.summary = summary;
-	this.content = content;
-	this.datePosted = datePosted;
-	this.author = author;
-};
-
-/* how to input new blog posts */
-var Post1 = new blogPost("Post1");
